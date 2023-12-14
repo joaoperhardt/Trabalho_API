b124794ef1a7f4d75d06a2e60b2fdd3c7c911dde
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2023-12-14 19:56:20 -0300

    add product interface and repository

---
+++ Trabalho_API_Produto/Contracts/Repository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Trabalho_API_Produto.DTO;
+using Trabalho_API_Produto.Entity;
+
+namespace Trabalho_API_Produto.Contracts.Repository
+{
+    public interface IProductRepository
+    {
+        Task Add(ProductDTO product);
+        Task<IEnumerable<ProductEntity>> Get();
+        Task Update(ProductEntity product);
+        Task Delete(int id);
+        Task<ProductEntity> GetById(int id);
+        Task<ProductEntity> GetByName(string name);
+    }
+}
---
+++ Trabalho_API_Produto/Repository/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using Dapper;
+using Trabalho_API_Produto.Contracts.Repository;
+using Trabalho_API_Produto.DTO;
+using Trabalho_API_Produto.Entity;
+using Trabalho_API_Produto.Infrastructure;
+
+namespace Trabalho_API_Produto.Repository
+{
+    public class ProductRepository : Connection, IProductRepository
+    {
+        public async Task Add(ProductDTO product)
+        {
+            string sql = "INSERT INTO PRODUCT (Name, Description, OriginalPrice, CurrentPrice, Discount, Buyers) VALUE (@Name, @Description, @OriginalPrice, @CurrentPrice, @Discount, @Buyers)";
+
+            await Execute(sql, product);
+        }
+
+        public async Task Delete(int id)
+        {
+            string sql = "DELETE FROM PRODUCT WHERE Id = @id";
+            await Execute(sql, new { id });
+        }
+
+        public async Task<IEnumerable<ProductEntity>> Get()
+        {
+            string sql = "SELECT * FROM PRODUCT";
+            return await GetConnection().QueryAsync<ProductEntity>(sql);
+        }
+
+        public async Task<ProductEntity> GetById(int id)
+        {
+            string sql = "SELECT * FROM PRODUCT WHERE Id = @id";
+            return await GetConnection().QueryFirstAsync<ProductEntity>(sql, new { id });
+        }
+
+        public async Task<ProductEntity> GetByName(string name)
+        {
+            string sql = "SELECT * FROM PRODUCT WHERE Name = @name";
+            return await GetConnection().QueryFirstAsync<ProductEntity>(sql, new { name });
+        }
+
+        public async Task Update(ProductEntity product)
+        {
+            string sql = @"
+                UPDATE PRODUCT SET 
+                Name = @Name, 
+                Description = @Description, 
+                OriginalPrice = @OriginalPrice, 
+                CurrentPrice = @CurrentPrice, 
+                Discount = @Discount, 
+                Buyers = @Buyers 
+                WHERE Id = @Id";
+
+            await Execute(sql, product);
+        }
+    }
+}
