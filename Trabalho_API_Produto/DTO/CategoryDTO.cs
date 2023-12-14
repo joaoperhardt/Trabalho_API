148b3de7802ff8a93049e76b74b754ef67de30cc
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2023-12-14 20:15:17 -0300

    add category entity

---
+++ Trabalho_API_Produto/DTO/CategoryDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Trabalho_API_Produto.DTO
+{
+    public class CategoryDTO
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Trabalho_API_Produto/Entity/CategoryEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Trabalho_API_Produto.Entity
+{
+    public class CategoryEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
