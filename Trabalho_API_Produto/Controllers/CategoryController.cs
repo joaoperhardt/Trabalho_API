c340a46ba9609f04d8e55d8c37f39ecf871c348d
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2023-12-14 20:56:35 -0300

    add category repository and contoller

---
+++ Trabalho_API_Produto/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trabalho_API_Produto.Contracts.Repository;
+
+namespace Trabalho_API_Produto.Controllers
+{
+    [ApiController]
+    [Route("category")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _categoryRepository.Get());
+        }
+    }
+}
--- Trabalho_API_Produto/Program.cs
+++ Trabalho_API_Produto/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddTransient<IProductRepository, ProductRepository>();
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddSwaggerGen();
 
