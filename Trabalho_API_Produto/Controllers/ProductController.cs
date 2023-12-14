6d8860aca1d1beb1dae937283311307d70e5b1d3
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2023-12-14 20:10:15 -0300

    add controller and transient

---
+++ Trabalho_API_Produto/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Trabalho_API_Produto.Contracts.Repository;
+using Trabalho_API_Produto.DTO;
+using Trabalho_API_Produto.Entity;
+
+namespace Trabalho_API_Produto.Controllers
+{
+    [ApiController]
+    [Route("product")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductDTO product)
+        {
+            await _productRepository.Add(product);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _productRepository.Get());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            return Ok(await _productRepository.GetById(id));
+        }
+
+        [HttpGet("ByName/{name}")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            return Ok(await _productRepository.GetByName(name));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(ProductEntity product)
+        {
+            await _productRepository.Update(product);
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _productRepository.Delete(id);
+            return Ok();
+        }
+    }
+}
--- Trabalho_API_Produto/Program.cs
+++ Trabalho_API_Produto/Program.cs
@@ -1,3 +1,6 @@
+using Trabalho_API_Produto.Contracts.Repository;
+using Trabalho_API_Produto.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
