6a4fea7551ec4a72127acb850adf6b7b566ad76d
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2023-12-14 19:49:05 -0300

    add product entity and dto

---
+++ Trabalho_API_Produto/DTO/ProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Trabalho_API_Produto.DTO
+{
+    public class ProductDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double OriginalPrice { get; set; }
+        public double CurrentPrice { get; set; }
+        public double Discount { get; set; }
+        public int Buyers { get; set; }
+    }
+}
---
+++ Trabalho_API_Produto/Entity/ProductEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Trabalho_API_Produto.Entity
+{
+    public class ProductEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double OriginalPrice { get; set; }
+        public double CurrentPrice { get; set; }
+        public double Discount { get; set; }
+        public int Buyers { get; set; }
+    }
+}
