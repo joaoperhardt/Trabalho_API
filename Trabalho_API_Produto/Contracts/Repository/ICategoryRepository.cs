2b378860e00842f923fabf8244a1e95cfc30a75c
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2023-12-14 20:44:46 -0300

    add category repository and interface

---
+++ Trabalho_API_Produto/Contracts/Repository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Trabalho_API_Produto.Entity;
+
+namespace Trabalho_API_Produto.Contracts.Repository
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<CategoryEntity>> Get();
+    }
+}
---
+++ Trabalho_API_Produto/Repository/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Dapper;
+using Trabalho_API_Produto.Contracts.Repository;
+using Trabalho_API_Produto.Entity;
+using Trabalho_API_Produto.Infrastructure;
+
+namespace Trabalho_API_Produto.Repository
+{
+    public class CategoryRepository : Connection, ICategoryRepository
+    {
+        public async Task<IEnumerable<CategoryEntity>> Get()
+        {
+            string sql = "SELECT * FROM CATEGORY";
+            return await GetConnection().QueryAsync<CategoryEntity>(sql);
+        }
+    }
+}
