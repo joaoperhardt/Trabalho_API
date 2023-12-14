da6d2742e8af2603e7763129d0c140a18f0cacde
JOAO PEDRO ERHARDT <joao.erhardt@alunos.sc.senac.br>
2023-12-14 20:58:56 -0300

    add store entity and dto

---
+++ Trabalho_API_Produto/DTO/StoreDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Trabalho_API_Produto.DTO
+{
+    public class StoreDTO
+    {
+        public string Name { get; set; }
+        public int Cep { get; set; }
+        public int Cnpj { get; set; }
+    }
+}
---
+++ Trabalho_API_Produto/Entity/StoreEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Trabalho_API_Produto.Entity
+{
+    public class StoreEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cep { get; set; }
+        public int Cnpj { get; set; }
+    }
+}
